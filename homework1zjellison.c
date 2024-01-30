d64c8c9835e6a8eeda3b5b2b1ea799d56062cdf3
ZacharyJellison <zachary_jellison@student.uml.edu>
2024-01-30 00:45:00 -0500

    update

--- homework1zjellison.c
+++ homework1zjellison.c
@@ -13,6 +13,11 @@ static void pop();
 //QUEUE FUNCTIONS
 static void enqueue();
 static void dequeue();
+//LINKED LIST FUNCTIONS
+void pushLL();
+void sortedInsert();
+void insertionSort();
+void printlist();
 
 //**********  IMPLEMENTING THE STACK  **********
 //Reference: https://stackoverflow.com/questions/71717509/im-trying-to-create-a-stack-in-c-using-structures-but-my-push-function-doesnt
@@ -53,6 +58,61 @@ static void dequeue(QUEUE* q){
     q->rear -= 1;
 }
 
+//********** IMPLEMENTING THE LINKED LIST **********
+//Sorting Algorithm of Linked List https://www.geeksforgeeks.org/sorting-a-singly-linked-list/
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+struct node *sorted = NULL;
+
+void pushLL(int val){
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = val;
+    newnode->next = head;
+    head = newnode;
+    
+}
+
+void sortedInsert(struct node* newnode){
+    if (sorted == NULL || sorted->data >= newnode->data){
+        newnode->next = sorted;
+        sorted = newnode;
+    }
+    else{
+        struct node* current = sorted;
+        
+        while (current->next != NULL && current->next->data < newnode->data){
+            current = current->next;
+        }
+
+        newnode->next = current->next;
+        current->next = newnode;
+    }
+}
+
+void insertionSort(){
+    struct node* current = head;
+    
+    while (current != NULL){
+        struct node* next = current->next;
+        sortedInsert(current);
+        current = next;
+    }
+
+    head = sorted;
+}
+
+void printlist(){
+    while (head != NULL){
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
 int main(int argc, char **argv){
 //*****Reference for reading file input https://stackoverflow.com/questions/20378430/reading-numbers-from-a-text-file-into-an-array-in-c
 
@@ -77,10 +137,12 @@ int main(int argc, char **argv){
     printf("STACK CONTENTS:\n");
     stackprint(&arr);
     printf("LIST CONTENTS\n");
-    //listprint(&arr);
+    listprint(&arr);
 
     return 0;
 }
+
+
 
 
 //*****Function Definitions*****
@@ -126,10 +188,12 @@ void queueprint(int QinpArray[]){
 //Prints in Numerical order (Smalled to Largest)
 void listprint(int LinpArray[]){
 //Move Inputs from array to Data Structure
+    for(int i = 0; i < 10; i++){
+        pushLL(LinpArray[i]);
+    }
 
 //Print Contents
-    for(int i = 0; i < 10; i++){
-        printf("%d ", LinpArray[i]);
-    }
+    insertionSort(head);
+    printlist(head);
     printf("\n");
 }
