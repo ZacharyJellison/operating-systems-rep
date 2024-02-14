57612a7761223bae13e36e59ccd49c4ff8557c4c
ZacharyJellison <zachary_jellison@student.uml.edu>
2024-02-14 18:49:43 -0500

    finished fork-join

---
+++ IndividualProgram2/fork-join.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "common_threads.h"
+
+sem_t s; 
+
+void *child(void *arg) {
+    printf("child\n");
+    sem_post(&s);                               // use semaphore here
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t p;
+    printf("parent: begin\n");
+    sem_init(&s, 0, 0);                         // init semaphore here
+    Pthread_create(&p, NULL, child, NULL);
+    sem_wait(&s);                               // use semaphore here
+    printf("parent: end\n");
+    return 0;
+}
