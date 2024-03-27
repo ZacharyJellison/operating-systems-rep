6be19993d365bf8d5f2872b24d5914d215f10ad3
ZacharyJellison <zachary_jellison@student.uml.edu>
2024-03-26 23:59:10 -0400

    STCF finished

--- Project3/sched_sim.c
+++ Project3/sched_sim.c
@@ -243,7 +243,7 @@ void *SJF(void *args) {
     }
     fprintf(functionInfo->output, "\nContext switches: %d\n\n\n", contextSwitches);
 
-    functionInfo->simData->contextSwitch[0] = contextSwitches;
+    functionInfo->simData->contextSwitch[1] = contextSwitches;
 
     free(process_sequence);
     free(tempBurst);
@@ -285,14 +285,12 @@ void *SJF(void *args) {
 void *STCF(void *args){
     passed_Info *functionInfo = (passed_Info *)args;
     int current_time = 0;
-    float waiting_time = 0;
-    float turnaround_time = 0;
     int contextSwitches = 0;
-    int* process_sequence = (int*)malloc(functionInfo->simData->size * sizeof(int));
+    int process[100];
     int* WT = (int*)malloc(functionInfo->simData->size * sizeof(int));
     int* TT = (int*)malloc(functionInfo->simData->size * sizeof(int));
 
-    fprintf(functionInfo->output, "***** SJF Scheduling *****\n");
+    fprintf(functionInfo->output, "***** STCF Scheduling *****\n");
 
     int n = functionInfo->simData->size;
     int* tempBurst = (int*)malloc(n * sizeof(int));
@@ -301,6 +299,8 @@ void *STCF(void *args){
     }
 
     int i = 0;
+    int p = 0;
+    process[0] = 0;
     int interval = functionInfo->simData->interval; // Interval for periodic updates
 
     // Print initial state
@@ -308,6 +308,8 @@ void *STCF(void *args){
     fprintf(functionInfo->output, "CPU: Loading process %d (remaining CPU burst = %d)\n", i, tempBurst[i]);
     fprintf(functionInfo->output, "Ready queue: %d", i);
     fprintf(functionInfo->output, "\n\n");
+
+    int last_job = 0;
 
     while (1) {
         int shortest_job = -1;
@@ -320,26 +322,27 @@ void *STCF(void *args){
                     shortest_job = j;
                     shortest_burst = tempBurst[j];
                 }
+                
             }
+
+            if(last_job != shortest_job){
+                contextSwitches++;
+                process[p+1] = shortest_job;
+                p += 1;
+            }
+
+            last_job = shortest_job;
 
             if(shortest_job == -1)
                 break;
 
         // Update time and execute the shortest job
-        //current_time += tempBurst[shortest_job];
         current_time += 1;
         tempBurst[shortest_job] -= 1;
 
         // Update waiting and turnaround time for the completed job
         WT[shortest_job] = current_time - functionInfo->simData->arrivalTime[shortest_job] - functionInfo->simData->CPU_Burst[shortest_job];
         TT[shortest_job] = current_time - functionInfo->simData->arrivalTime[shortest_job];
-        process_sequence[i] = shortest_job;
-
-        // Move to the next process
-        if (tempBurst[i] <= 0 && i < n) {
-            i++; // Move to the next process
-            contextSwitches++;
-        }
 
         if (i >= n)
             break;
@@ -348,46 +351,59 @@ void *STCF(void *args){
         if (current_time % interval == 0) {
             // Print periodic update
             fprintf(functionInfo->output, "t = %d\n", current_time);
-            fprintf(functionInfo->output, "CPU: Running process %d (remaining CPU burst = %d)\n", shortest_job, tempBurst[shortest_job]);
+            fprintf(functionInfo->output, "CPU: Running process %d (remaining CPU burst = %d)", shortest_job, tempBurst[shortest_job]);
+            fprintf(functionInfo->output, "\n");
             fprintf(functionInfo->output, "Ready queue: ");
+            int count = 0;
             for (int q = 0; q < n; q++) {
-                if (tempBurst[q] > 0 && functionInfo->simData->arrivalTime[q] <= current_time) {
-                    fprintf(functionInfo->output, "%d", q);
-                    if (q < n - 1)
-                        fprintf(functionInfo->output, "-");
+                if (tempBurst[q] > 0 && functionInfo->simData->arrivalTime[q] <= current_time && (q != shortest_job)) {
+                    fprintf(functionInfo->output, "%d ", q);
+                    count ++;
                 }
+            }
+            if(count == 0){
+                fprintf(functionInfo->output, "empty");
             }
             fprintf(functionInfo->output, "\n\n");
         }
     }
 
+    int* process_sequence = (int*)malloc(contextSwitches * sizeof(int));
+    for(int y = 0; y < contextSwitches; y++){
+        process_sequence[y] = process[y];
+    }
+
     // Calculate average waiting and turnaround time
+    float waiting_time = 0;
+    float turnaround_time = 0;
     for (int k = 0; k < n; k++) {
         waiting_time += WT[k];
     }
-    functionInfo->simData->AVGwaitTime[1] = waiting_time / n;
+    functionInfo->simData->AVGwaitTime[2] = waiting_time / n;
 
     for (int k = 0; k < n; k++) {
         turnaround_time += TT[k];
     }
-    functionInfo->simData->AVGturnTime[1] = turnaround_time / n;
+    functionInfo->simData->AVGturnTime[2] = turnaround_time / n;
 
 
     // Print summary
     fprintf(functionInfo->output, "*********************************************************\n");
-    fprintf(functionInfo->output, "SJF Summary (WT = wait time, TT = turnaround time):\n\n");
+    fprintf(functionInfo->output, "STCF Summary (WT = wait time, TT = turnaround time):\n\n");
     fprintf(functionInfo->output, "PID\tWT\tTT\n");
     for (int j = 0; j < n; j++) {
         fprintf(functionInfo->output, " %d\t%d\t%d\n", j, WT[j], TT[j]);
     }
-    fprintf(functionInfo->output, "AVG\t%.2f\t%.2f\n", functionInfo->simData->AVGwaitTime[1], functionInfo->simData->AVGturnTime[1]);
+    fprintf(functionInfo->output, "AVG\t%.2f\t%.2f\n", functionInfo->simData->AVGwaitTime[2], functionInfo->simData->AVGturnTime[2]);
     fprintf(functionInfo->output, "\nProcess sequence: ");
-    for (int j = 0; j < n; j++) {
-        fprintf(functionInfo->output, "%d ", process_sequence[j]);
+    for (int j = 0; j < contextSwitches; j++) {
+        fprintf(functionInfo->output, "%d", process_sequence[j]);
+        if (j < contextSwitches - 1)
+            fprintf(functionInfo->output, "-");
     }
     fprintf(functionInfo->output, "\nContext switches: %d\n\n\n", contextSwitches);
 
-    functionInfo->simData->contextSwitch[0] = contextSwitches;
+    functionInfo->simData->contextSwitch[2] = contextSwitches;
 
     free(process_sequence);
     free(tempBurst);
@@ -445,21 +461,21 @@ void *Summary(void *args){
     fprintf(functionInfo->output, "Wait Time Comparison\n");
     fprintf(functionInfo->output, "1 %s\t\t%.2lf\n", sortingNames[0], functionInfo->simData->AVGwaitTime[0]);
     fprintf(functionInfo->output, "2 %s\t\t%.2lf\n", sortingNames[1], functionInfo->simData->AVGwaitTime[1]);
-    fprintf(functionInfo->output, "3 %s\t\t%.2lf\n", sortingNames[2], 0.1);
+    fprintf(functionInfo->output, "3 %s\t\t%.2lf\n", sortingNames[2], functionInfo->simData->AVGwaitTime[2]);
     fprintf(functionInfo->output, "4 %s\t\t%.2lf\n", sortingNames[3], 0.1);
     fprintf(functionInfo->output, "5 %s\t\t%.2lf\n\n", sortingNames[4], 0.1);
 
     fprintf(functionInfo->output, "Turnaround Time Comparison\n");
     fprintf(functionInfo->output, "1 %s\t\t%.2lf\n", sortingNames[0], functionInfo->simData->AVGturnTime[0]);
     fprintf(functionInfo->output, "2 %s\t\t%.2lf\n", sortingNames[1], functionInfo->simData->AVGturnTime[1]);
-    fprintf(functionInfo->output, "3 %s\t\t%.2lf\n", sortingNames[2], 0.1);
+    fprintf(functionInfo->output, "3 %s\t\t%.2lf\n", sortingNames[2], functionInfo->simData->AVGturnTime[2]);
     fprintf(functionInfo->output, "4 %s\t\t%.2lf\n", sortingNames[3], 0.1);
     fprintf(functionInfo->output, "5 %s\t\t%.2lf\n\n", sortingNames[4], 0.1);
 
     fprintf(functionInfo->output, "Context Switch Comparison\n");
     fprintf(functionInfo->output, "1 %s\t\t%d\n", sortingNames[0], functionInfo->simData->contextSwitch[0]);
     fprintf(functionInfo->output, "2 %s\t\t%d\n", sortingNames[1], functionInfo->simData->contextSwitch[1]);
-    fprintf(functionInfo->output, "3 %s\t\t%d\n", sortingNames[2], 1);
+    fprintf(functionInfo->output, "3 %s\t\t%d\n", sortingNames[2], functionInfo->simData->contextSwitch[2]);
     fprintf(functionInfo->output, "4 %s\t\t%d\n", sortingNames[3], 1);
     fprintf(functionInfo->output, "5 %s\t\t%d", sortingNames[4], 1);
 }
